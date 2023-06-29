8f06b9e706e075e6f72a12fbf67aea56305046c5
Samiwel Thomas <samiwel.thomas@digital.homeoffice.gov.uk>
2023-06-29 08:54:01 +0100

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+
+
+class Forum(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(100), unique=True, nullable=False)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
+                            nullable=False)
+    category = db.relationship('Category',
+                               backref=db.backref('forums', lazy=True))
